26163480ea8d17a6b942df8d72bd85d24d1edc36
shotasan <wondermoai@gmail.com>
2019-02-19 17:02:29 +0900

    新規投稿後に確認画面を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def confirm
+    @post = Post.new(post_params)
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :sessions, onlu:[:new,:create,:destroy]
   resources :users, only:[:new,:create,:show]
-  resources :posts
+  resources :posts do
+    collection do
+      post 'confirm'
+    end
+  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
