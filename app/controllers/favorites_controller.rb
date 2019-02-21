11369992438895e902e1efc4ebd990ff928d674a
shotasan <wondermoai@gmail.com>
2019-02-21 11:56:51 +0900

    ヘッダーとフッターを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,8 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.where(user_id: current_user.id)
+  end
+
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
     redirect_to post_path(params[:post_id]), notice: "#{favorite.post.user.name}さんのブログをお気に入り登録しました"
