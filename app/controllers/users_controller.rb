fb06bfe80c517d6184f3e84e08437baa9bba24b5
shotasan <wondermoai@gmail.com>
2019-02-22 14:47:25 +0900

    新規登録直後にログインするように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "登録に成功しました"
       redirect_to user_path(@user.id)
     else
