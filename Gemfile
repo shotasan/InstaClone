f5a1bcccc8f65921baf5e643f29efcde886f1f06
shotasan <wondermoai@gmail.com>
2019-02-22 13:07:44 +0900

    デザインを修正

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'bcrypt', '3.1.11'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick'
 gem 'faker'
+gem 'rails-i18n', '~> 5.1'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module InstaClone
     config.active_record.default_timezone = :local
 
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
